d6c8ade66e4aa82fb59f093762930807eb53d5c2
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-10 22:02:30 -0300

    create-service-repository-categoria

--- src/main/java/com/nelioalves/cursomcp/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomcp/resources/CategoriaResources.java
@@ -3,24 +3,27 @@ package com.nelioalves.cursomcp.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomcp.domain.Categoria;
+import com.nelioalves.cursomcp.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = Arrays.asList(cat1,cat2);
-		
-		return lista;
+
+	@Autowired
+	CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }

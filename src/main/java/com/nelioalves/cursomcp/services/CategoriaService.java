03eca4698441627a59e64e62983cb85de93dad34
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-03-11 08:52:45 -0300

    exceptions

--- src/main/java/com/nelioalves/cursomcp/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomcp/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomcp.domain.Categoria;
 import com.nelioalves.cursomcp.repositories.CategoriaRepository;
+import com.nelioalves.cursomcp.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
